31d376a5200fe596b1ef45eb5087e34d9ea4bbd8
ndmanh1201 <ndmanh121201@gmail.com>
2024-01-13 13:44:48 +0700

    add design system

--- app/src/main/java/com/app/mova/ui/theme/Color.kt
+++ app/src/main/java/com/app/mova/ui/theme/Color.kt
@@ -9,3 +9,32 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val PrimaryColor = Color(0xFF8612E2)
+val SecondaryColor = Color(0xFF8FFF00)
+
+val AppBackgroundDark = Color(0xFF181A20)
+val AppBackgroundLight = Color(0xFFFFFFFF)
+
+//dark color
+val Dark1 = Color(0xFF181A20)
+val Dark2 = Color(0xFF1F222A)
+val Dark3 = Color(0xFF35383F)
+
+//primaries
+val  Primary400 = Color(0xFFA941E8)
+val  Primary300 = Color(0xFFB771EE)
+val  Primary200 = Color(0xFFCEA0F3)
+val  Primary100 = Color(0xFFF3E7FC)
+
+//greyscale
+val Gray900 = Color(0xFF212121)
+val Gray800 = Color(0xFF424242)
+val Gray700 = Color(0xFF616161)
+val Gray600 = Color(0xFF757575)
+val Gray500 = Color(0xFF9E9E9E)
+val Gray400 = Color(0xFFBDBDBD)
+val Gray300 = Color(0xFFE0E0E0)
+val Gray200 = Color(0xFFEEEEEE)
+val Gray100 = Color(0xFFF5F5F5)
+val Gray50 = Color(0xFFFAFAFA)
--- app/src/main/java/com/app/mova/ui/theme/Type.kt
+++ app/src/main/java/com/app/mova/ui/theme/Type.kt
@@ -2,19 +2,61 @@ package com.app.mova.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.app.mova.R
+
+val fontFamily = FontFamily(
+    Font(R.font.livvic_bold, FontWeight.Bold),
+    Font(R.font.livvic_semi_bold, FontWeight.SemiBold),
+    Font(R.font.livvic_medium, FontWeight.Medium),
+    Font(R.font.livvic_regular, FontWeight.Normal),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    //body
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        letterSpacing = 0.5.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 12.sp,
+        letterSpacing = 0.5.sp
+    ),
+
+    //headline
+    headlineLarge = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
+        letterSpacing = 0.5.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+        letterSpacing = 0.5.sp
+    ),
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
